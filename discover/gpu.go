21a432c54c56e4f63e37b9a6cd5e26d08a8ff821
neverclover <evercloverxd@outlook.com>
2025-07-29 10:39:10 +0800

    adaptation for sm37

--- discover/gpu.go
+++ discover/gpu.go
@@ -66,7 +66,7 @@ var (
 // With our current CUDA compile flags, older than 5.0 will not work properly
 // (string values used to allow ldflags overrides at build time)
 var (
-	CudaComputeMajorMin = "5"
+	CudaComputeMajorMin = "3"
 	CudaComputeMinorMin = "0"
 )
 
